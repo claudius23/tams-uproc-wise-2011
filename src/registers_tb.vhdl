e388b8a35c585eeb7f7e2de66768b2802333a7a4
Claudius <claudius@ambtec.de>
2011-12-22 17:28:45 +0100

    adept testbench

--- src/registers_tb.vhdl
+++ src/registers_tb.vhdl
@@ -15,46 +15,53 @@ architecture behavior of registers_tb is
     generic (
       register_count : integer range 1 to 64 := register_count);
     port (
-      select_a, select_b : in integer range 0 to register_count;
-      value_a, value_b   : inout data_bus;
-      write              : in std_logic;  -- set to 1 to write
-      enabled            : in std_logic);  -- set to 1 to enable
+		register_select_in_a, register_select_in_b : in integer range 0 to register_count-1;
+		register_select_out_a, register_select_out_b : in integer range 0 to register_count-1;
+		register_value_in_a, register_value_in_b   : in  data_bus;
+		register_value_out_a, register_value_out_b   : out data_bus;
+		register_write_enabled                     : in std_logic;   -- set to 1 to write
+		clock 												  : in std_logic); -- clock
   end component;
 
   for registers_0: registers use entity work.registers;
 
-  signal select_a, select_b : integer range 1 to register_count;
-  signal value_a, value_b   : data_bus;
-  signal write              : std_logic;
-  signal enabled            : std_logic;
-
+  signal register_select_in_a, register_select_in_b : integer range 0 to register_count-1;
+  signal register_select_out_a, register_select_out_b : integer range 0 to register_count-1;
+  signal register_value_in_a, register_value_in_b   : data_bus;
+  signal register_value_out_a, register_value_out_b   : data_bus;
+  signal register_write_enabled              : std_logic;
+  
   begin
       registers_0: registers port map (
-        select_a => select_a,
-        select_b => select_b,
-        value_a  => value_a,
-        value_b  => value_b,
-        write    => write,
-        enabled  => enabled
+		register_select_in_a		=>	register_select_in_a,
+		register_select_in_b		=>	register_select_in_b,
+		register_select_out_a	=>	register_select_out_a,
+		register_select_out_b	=>	register_select_out_b,
+		register_value_in_a		=>	register_value_in_a,
+		register_value_in_b		=> register_value_in_b,
+		register_value_out_a		=>	register_value_out_a,
+		register_value_out_b		=> register_value_out_b,
+		register_write_enabled	=> register_write_enabled,
       );
 
       -- purpose: testbench for registers
       -- type   : sequential
       execute_test: process
         type registers_test is record         -- a single test
-          select_a, select_b : integer range 0 to register_count;
-          value_a, value_b   : data_bus;
+          register_select_in_a, register_select_in_b : integer range 0 to register_count-1;
+			 register_select_out_a, register_select_out_b : integer range 0 to register_count-1;
+          register_value_in_a, register_value_in_b   : data_bus;
+			 register_value_out_a, register_value_out_b   : data_bus;
         end record;
 
         type registers_tests is array (natural range <>) of registers_test;
         constant tests : registers_tests :=
           (
-            -- TODO: figure out why 0 results in a bounds error
-            (1, 2, zero_word, one_word),
-            (1, 2, one_word, zero_word),
-            (3, 4, two_word, zero_word),
-            (1, 2, zero_word, three_word),
-            (3, 4, zero_word, zero_word)
+            (0, 1, zero_word, one_word),
+            (0, 1, one_word, zero_word),
+            (2, 3, two_word, zero_word),
+            (0, 1, zero_word, three_word),
+            (2, 3, zero_word, zero_word)
           );
 
       begin  -- process
@@ -63,36 +70,32 @@ architecture behavior of registers_tb is
           wait for 1 ns;
 
           -- write phase
-          select_a <= tests(i).select_a;
-          select_b <= tests(i).select_b;
-          value_a <= tests(i).value_a;
-          value_b <= tests(i).value_b;
-          enabled <= '1';
-          write <= '1';
-
-          -- generate enabled pulse
-          enabled <= '1';
+          register_select_in_a <= tests(i).register_select_in_a;
+          register_value_in_a <= tests(i).register_value_in_a;
+			 register_write_enabled <= '1';
           wait for 1 ns;
 
+			 register_select_in_b <= tests(i).register_select_in_b;
+          register_value_in_b <= tests(i).register_value_in_b;
+			 register_write_enabled <= '1';
+          wait for 1 ns;
+          
           -- prepare for read phase, set bus on high impedance
-          value_a <= z_word;
-          value_b <= z_word;
-          enabled <= '0';
-          write <= '0';
+          --value_a <= z_word;
+          --value_b <= z_word;
+          --enabled <= '0';
+          --write <= '0';
 
-          enabled <= '0';
-          wait for 1 ns;
+          --enabled <= '0';
+          --wait for 1 ns;
 
           -- read phase
-          select_a <= tests(i).select_a;
-          select_b <= tests(i).select_b;
-          enabled <= '1';
-
+          register_select_out_a <= tests(i).register_select_out_a;
+          register_select_out_b <= tests(i).register_select_out_b;
+          
           -- generate enabled pulse
-          enabled <= '1';
-          wait for 1 ns;
-
-          assert value_a = tests(i).value_a and value_b = tests(i).value_b
+          
+          assert register_value_out_a = tests(i).register_value_out_a and register_value_out_b = tests(i).register_value_out_b
             report "bad result" severity error;
         end loop;
 
